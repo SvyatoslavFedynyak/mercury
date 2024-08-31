07a5db8eb1d8ce98d806b93c48af6e54768340a9
Svyatoslav Fedynyak <sfedynya@cisco.com>
2024-08-31 12:04:58 -0700

    Try with old scripts

--- src/cython/build-wheels.sh
+++ src/cython/build-wheels.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+set -x
+
 # docker run --rm -v /home/ubuntu/mercury-transition-clean:/mercury:rw quay.io/pypa/manylinux_2_28_x86_64:latest /mercury/src/cython/build-wheels.sh
 
+PYBIN=/usr/bin/python3
 
 function repair_wheel {
-    wheel="$1"
+    wheel=$1
+    plat=$2
     if ! auditwheel show "$wheel"; then
         echo "Skipping non-platform wheel $wheel"
     else
-        auditwheel repair "$wheel" --plat manylinux_2_28_x86_64 -w /mercury/src/cython/wheelhouse/
+        auditwheel repair "$wheel" --plat "$plat" -w $MERCURY_DIR/src/cython/wheelhouse/
     fi
 }
 
 # Install a system package required by our library
-yum install -y openssl-devel make zlib-devel
+#yum install -y openssl-devel make zlib-devel
 
 # configure and make mercury
-cd /mercury
+cd $MERCURY_DIR
 make clean
 ./configure
 make
@@ -27,19 +31,19 @@ FLAGS='-DSSLNEW'
 export ENV_CFLAGS=${FLAGS}
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PIPBIN in /usr/bin/pip[2-3].*; do
     # clean up cython directory
-    cd /mercury/src/cython
+    cd $MERCURY_DIR/src/cython
     make clean
     rm -r mercury_python.egg-info/
     rm -r build/
     cd -
 
-    "${PYBIN}/pip" install -r /mercury/src/cython/requirements.txt
-    "${PYBIN}/pip" wheel /mercury/src/cython/ --no-deps -w /mercury/src/cython/wheelhouse/
+    $PIPBIN install -r $MERCURY_DIR/src/cython/requirements.txt
+    $PIPBIN wheel $MERCURY_DIR/src/cython/ --no-deps -w $MERCURY_DIR/src/cython/wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in /mercury/src/cython/wheelhouse/*.whl; do
-    repair_wheel "$whl"
+for whl in $MERCURY_DIR/src/cython/wheelhouse/*.whl; do
+    repair_wheel "$whl" $1
 done
